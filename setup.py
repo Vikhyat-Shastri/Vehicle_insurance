b85f9e8b4f82d0126ccb39cbe77e4a95d2c62dbf
Shastri-727 <xiavikhyatshastri@gmail.com>
2025-11-19 18:46:37 +0530

    Update project with author information and fix GitHub Actions workflow

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Vikhyat Shastri",
+    author_email="xiavikhyatshastri@gmail.com",
     packages=find_packages()
 )
